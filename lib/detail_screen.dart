1196218fea28c2daabb66a724d6e54ff8442ef37
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-06 22:42:44 +0700

    refactor(main): make dynamic data on screen

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_bandung/model/tourism_place.dart';
 
 var informationTextStyle = TextStyle(fontFamily: 'Oxygen');
 
 class DetailScreen extends StatelessWidget {
+
+  final TourismPlace place;
+
+  DetailScreen({required this.place});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,11 +17,11 @@ class DetailScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Image.asset('images/farm-house.jpg'),
+                Image.asset(place.imageAsset),
                 Container(
                   margin: EdgeInsets.only(top: 16.0),
-                  child: const Text(
-                    'Farm House Lembang',
+                  child: Text(
+                    place.name,
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       fontFamily: 'Staatliches',
@@ -32,7 +38,7 @@ class DetailScreen extends StatelessWidget {
                         children: <Widget>[
                           Icon(Icons.calendar_today),
                           SizedBox(height: 10.0),
-                          Text('Open Everyday',
+                          Text(place.openDays,
                             style: informationTextStyle,
                           ),
                         ],
@@ -41,7 +47,7 @@ class DetailScreen extends StatelessWidget {
                         children: <Widget>[
                           Icon(Icons.access_time),
                           SizedBox(height: 10.0),
-                          Text('09:00 - 20:00',
+                          Text(place.openTime,
                             style: informationTextStyle,
                           ),
                         ],
@@ -51,7 +57,7 @@ class DetailScreen extends StatelessWidget {
                           Icon(Icons.monetization_on),
                           SizedBox(height: 10.0),
                           Text(
-                            'Rp. 25.000',
+                            place.ticketPrice,
                             style: informationTextStyle,
                           ),
                         ],
@@ -61,8 +67,8 @@ class DetailScreen extends StatelessWidget {
                 ),
                 Container(
                   padding: EdgeInsets.all(16.0),
-                  child: const Text(
-                    'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                  child: Text(
+                    place.description,
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       fontFamily: 'Oxygen',
@@ -74,35 +80,17 @@ class DetailScreen extends StatelessWidget {
                   height: 150,
                   child: ListView(
                     scrollDirection: Axis.horizontal,
-                    children: [
-                      Padding(
+                    children: place.imageUrls.map((url) {
+                      return Padding(
                         padding: const EdgeInsets.all(4.0),
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(20),
                           child: Image.network(
-                              'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'
+                              url
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.network(
-                              'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.network(
-                              'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'
-                          ),
-                        ),
-                      ),
-                    ],
+                      );
+                    }).toList(),
                   ),
                 ),
               ],

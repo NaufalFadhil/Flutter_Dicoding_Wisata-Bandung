6652ab448bebb8c29a3571b5fa2a423e321c9f24
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-10 18:33:27 +0700

    refactor(detail): make responsive design

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -11,6 +11,25 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth > 800) {
+            return DetailWebPage(place: place);
+          } else {
+            return DetailMobilePage(place: place);
+          }
+        }
+    );
+  }
+}
+
+class DetailMobilePage extends StatelessWidget {
+  final TourismPlace place;
+
+  const DetailMobilePage({required this.place});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
           child: SingleChildScrollView(
@@ -29,13 +48,13 @@ class DetailScreen extends StatelessWidget {
                               CircleAvatar(
                                 backgroundColor: Colors.grey,
                                 child: IconButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    icon: Icon(
-                                      Icons.arrow_back,
-                                      color: Colors.white,
-                                    ),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  icon: Icon(
+                                    Icons.arrow_back,
+                                    color: Colors.white,
+                                  ),
                                 ),
                               ),
                               FavoriteButton(),
@@ -127,6 +146,163 @@ class DetailScreen extends StatelessWidget {
     );
   }
 }
+
+class DetailWebPage extends StatefulWidget {
+  final TourismPlace place;
+
+  const DetailWebPage({required this.place});
+
+  @override
+  State<DetailWebPage> createState() => _DetailWebPageState();
+}
+
+class _DetailWebPageState extends State<DetailWebPage> {
+  final _scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 16,
+            horizontal: 64,
+          ),
+          child: Center(
+            child: Container(
+              width: 1200,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Wisata Bandung',
+                    style: TextStyle(
+                      fontFamily: 'Staatliches',
+                      fontSize: 32,
+                    ),
+                  ),
+                  SizedBox(height: 32),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                child: Image.asset(widget.place.imageAsset),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              SizedBox(height: 16),
+                              Scrollbar(
+                                isAlwaysShown: true,
+                                controller: _scrollController,
+                                child: Container(
+                                  height: 150,
+                                  padding: EdgeInsets.only(bottom: 16),
+                                  child: ListView(
+                                    controller: _scrollController,
+                                    scrollDirection: Axis.horizontal,
+                                    children: widget.place.imageUrls.map((url) {
+                                      return Padding(
+                                        padding: const EdgeInsets.all(4.0),
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(10),
+                                          child: Image.network(url),
+                                        ),
+                                      );
+                                    }).toList(),
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
+                      ),
+                      SizedBox(width: 32),
+                      Expanded(
+                          child: Card(
+                            child: Container(
+                              padding: const EdgeInsets.all(16),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Container(
+                                    child: Text(
+                                      widget.place.name,
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontSize: 30.0,
+                                        fontFamily: 'Staatliches',
+                                      ),
+                                    ),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Icon(Icons.calendar_today),
+                                          SizedBox(width: 8.0),
+                                          Text(
+                                            widget.place.openDays,
+                                            style: informationTextStyle,
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  SizedBox(height: 8.0),
+                                  Row(
+                                    children: [
+                                      Icon(Icons.access_time),
+                                      SizedBox(width: 8.0),
+                                      Text(
+                                        widget.place.openTime,
+                                        style: informationTextStyle,
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 8.0),
+                                  Row(
+                                    children: [
+                                      Icon(Icons.monetization_on),
+                                      SizedBox(width: 8.0),
+                                      Text(
+                                        widget.place.ticketPrice,
+                                        style: informationTextStyle,
+                                      ),
+                                    ],
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                                    child: Text(
+                                      widget.place.description,
+                                      textAlign: TextAlign.justify,
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontFamily: 'Oxygen',
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        )
+    );
+  }
+
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+}
+
 
 class FavoriteButton extends StatefulWidget {
   const FavoriteButton({Key? key}) : super(key: key);
